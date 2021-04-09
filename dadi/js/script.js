6ac5c5496ec314398564ddedb07d2214331b8890
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-09 09:45:48 +0200

    dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,16 @@
+
+//player
+var player1 = Math.floor(Math.random() * 6) +1;
+console.log("Player dice roll: " + player1)
+//computer
+var npc = Math.floor(Math.random() * 6) +1;
+console.log("computer: " + npc)
+
+//result game
+if(player1 > npc){
+  console.log("You win!")
+} else if(npc > player1){
+  console.log("You loose!")
+}else{
+  console.log("Pari")
+}
